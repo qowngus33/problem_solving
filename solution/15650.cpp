287243dec18a72fb5be51e0bebf43d3ab48cfbf7
qowngus33 <83813866+qowngus33@users.noreply.github.com>
2022-07-06 20:51:34 +0900

    added 15649 15650 15651 problem solution

---
+++ solution/15650.cpp
@@ -0,0 +1,38 @@
+//
+//  15650.cpp
+//  baekjoon
+//
+//  Created by 배주현 on 2022/07/06.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M, visited[10];
+vector<int> v;
+
+void backtrack(int cnt, int idx) {
+    if(cnt == M) {
+        for(int i=0;i<M;i++) printf("%d ",v[i]);
+        printf("\n");
+        return;
+    }
+    for(int i=idx;i<=N;i++) {
+        if(!visited[i]) {
+            visited[i] = true;
+            v.push_back(i);
+            backtrack(cnt+1, i);
+            v.erase(v.end()-1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main(void) {
+    scanf("%d%d",&N,&M);
+    backtrack(0,1);
+}
+
